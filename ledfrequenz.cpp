6c987cc1bb74cb4ca9f7548be4286a95d54cc542
bertus37987 <willyebert@gmx.de>
2025-10-10 10:37:33 +0200

    Update ledfrequenz.cpp

--- ledfrequenz.cpp
+++ ledfrequenz.cpp
@@ -1,5 +1,5 @@
 const int sensor = A0;
-int messWert;
+int messwert;
 const int led = 13;
 float wartezeit;
 
@@ -10,16 +10,14 @@ pinMode(led,OUTPUT);
 }
 
 void loop() {
-messWert = analogRead(sensor); //wiederstand wird gelesen
+messwert = analogRead(sensor); //wiederstand wird gelesen
 Serial.println(messwert);      //messwert wird in das terminal gedruckt
 
- float wartezeit = messwert / 2.0; //das delay wird ausgerechnet
+ float wartezeit = messwert / 20.0; //das delay wird ausgerechnet
  
-for(int i = 0; i < messwert; i++); //wird mal den wiederstand wiederholt
-{
-  digitalWrite(led,HIGH);
-  delay(wartezeit);       //das ist die wartezeit ehöht die frequenz
-  digitalWrite(led,LOW);
-  delay(wartezeit);
-}
+ digitalWrite(led,HIGH);
+delay(wartezeit);       //das ist die wartezeit ehöht die frequenz
+digitalWrite(led,LOW);
+delay(wartezeit);
+
 }
